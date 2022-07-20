bfeb0036032607cea5b6f0fd49a92b3de0b1b461
IncomingAnonymous <boluwatifetella@gmail.com>
2022-07-20 01:12:19 +0100

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
-  <h1>{{title}}</h1>
-  <p>Hello</p>
+  <h1 class="text-center">{{ title }}: {{clubsCount}}</h1>
+  <ul>
+    <li v-for="(team, index) in clubs" :key="team.name">
+      <div>{{ team.name }} <button @click="remove(index)">x</button></div>
+    </li>
+  </ul>
+  <form @submit.prevent="addClubs">
+    <input v-model="newClub" placeholder="Type your club">
+    <button type="submit" >Add Club</button>
+  </form>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        title: "Tella",
-      }
+export default {
+  computed: {
+    clubsCount() {
+      return this.clubs.length;
     }
+  },
+  data() {
+    return {
+      newClub: "",
+      title: "Clubs",
+      clubs: [
+        {name: "Chelsea"},
+        {name: "Man U"},
+        {name: "Arsenal"},
+        {name: "Man City"},
+        {name: "Liverpool"},
+      ],
+    };
+  },
+  methods:{
+    addClubs() {
+      if (this.newClub !== '') {
+        this.clubs.push({name: this.newClub});
+        this.newClub = '';
+      }
+    },
+    remove(index) {
+      this.clubs = this.clubs.filter((team, i) => i != index)
+    },
   }
+}
 </script>
